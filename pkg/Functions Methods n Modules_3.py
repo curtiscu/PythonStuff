45fa5c733b47e54e80fe5b90d1edc766bb7f3a95
curtiscu <github.com@drumgit.com>
2019-01-15 21:46:09 +0000

    finishing up the bit about working with modules..

--- pkg/Functions Methods n Modules_3.py
+++ pkg/Functions Methods n Modules_3.py
@@ -11,7 +11,10 @@ are many commonly used modules in python. It is worth noting that
 some modules are only available for Python 2.X and others are
 available for Python 3.
 
+See: https://docs.python.org/3.7/py-modindex.html
+
 '''
+
 
 # utility function by curtiscu
 # nice/ pretty print stuff ...
@@ -20,3 +23,33 @@ def pp(tab_count, str):
 
 
 pp(0,'#=== pg.8 start ===')
+
+import sys
+import os
+import random
+import math
+
+pp(0,'==== SYS ====')
+pp(1,'flegs: {0}'.format(sys.flags))
+pp(1,'base_prefix: {0}'.format(sys.base_prefix))
+#pp(0,'copyright: {0}'.format(sys.copyright))
+pp(1,'platform: {0}'.format(sys.platform))
+pp(1,'version: {0}'.format(sys.version))
+pp(1,'path: {0}'.format(sys.path))
+pp(1,'threadinfo: {0}\n'.format(sys.flags))
+
+pp(0,'==== OS ====')
+pp(1,'environ: {0}'.format(os.environ))
+pp(1,'getcwd: {0}'.format(os.getcwd()))
+pp(1,'getpid: {0}'.format(os.getpid()))
+
+pp(0,'\n==== RANDOM ====')
+pp(1,'random(): {0}'.format(random.random()))
+pp(1,'randint(2, 7): {0}'.format(random.randint(2, 7)))
+#pp(1,'getstate(): {0}'.format(random.getstate()))
+
+
+pp(0,'\n==== MATH ====')
+pp(1,'factorial(6): {0}'.format(math.factorial(6)))
+pp(1,'pi: {0}'.format(math.pi))
+pp(1,'e: {0}'.format(math.e))
