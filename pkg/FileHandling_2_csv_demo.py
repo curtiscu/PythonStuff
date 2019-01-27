21604f874235d55ee687c5516467cb882159861c
curtiscu <github.com@drumgit.com>
2019-01-27 16:54:42 +0000

    csv file parsing

--- pkg/FileHandling_2_csv_demo.py
+++ pkg/FileHandling_2_csv_demo.py
@@ -1,2 +1,49 @@
 
 
+'''
+
+Useful doc...
+
+    https://docs.python.org/3/library/csv.html \n
+
+
+'''
+
+import csv
+
+# utility function by curtiscu
+# nice/ pretty print stuff ...
+def pp(tab_count, str):
+    print('\t'*tab_count, str)
+
+
+def count_log_entries():
+
+    # specify counties to search for here ...
+    county_text1 = 'MARION COUNTY'
+    county_text2 = 'PINELLAS COUNTY'
+
+    pp(0,' \nSearch for counties {0}, {1}'.format(county_text1, county_text2))
+    with open('FL_insurance_sample.csv', newline='\n') as csvfile:
+        insurance_data = csv.reader(csvfile, delimiter = ',')
+        county_count1 = 0
+        county_count2 = 0
+
+        for row in insurance_data:
+            if (insurance_data.line_num == 1):
+                pp(0,'\n...found first line: {}'.format(row))
+                continue
+
+            if (row[2] == county_text1):
+                #pp(1,'{0} found @ line: {1}'.format(county_text1, insurance_data.line_num))
+                county_count1 += 1
+            elif (row[2] == county_text2):
+                #pp(1, '{0} found @ line: {1}'.format(county_text2, insurance_data.line_num))
+                county_count2 += 1
+
+        pp(0,'\n{0}: {1}, {2}: {3}'.format(county_text1, county_count1, county_text2, county_count2))
+
+
+
+count_log_entries()
+
--- pkg/FileHandling_2_log_parser.py
+++ pkg/FileHandling_2_log_parser.py
@@ -13,4 +13,38 @@ def pp(tab_count, str):
 
 
 
-pp(0,'Testing access utility function, see this, it works!!')
+def read_ssh_log():
+
+    log_entries = open("ssh_logon_attempt.txt").readlines()
+
+    total_log_counter = 0
+    failed_counter = 0
+    illegal_user_counter = 0
+    err_counter = 0
+
+    pp(0, '\nPrinting comments, generating counter totals...\n')
+
+    for log_entry in log_entries:
+        comments = log_entry[36:] # keeps only the english textual description, 2nd part of line
+        non_comments = log_entry[:35] # keeps 1st part of line
+        total_log_counter += 1
+
+        pp(1,'{0} - {1}'.format(total_log_counter, comments))
+
+        # gather counter totals...
+        if (comments[0] == 'F'): # 'Failed..'
+            failed_counter += 1
+        elif (comments[0] == 'I'): # 'Illegal..'
+            illegal_user_counter += 1
+        else: # 'error..'/ other
+            err_counter += 1
+
+
+    print('\n')
+    pp(0,'Failed - {}'.format(failed_counter))
+    pp(0,'Illegal - {}'.format(illegal_user_counter))
+    pp(0,'error /other - {}'.format(err_counter))
+
+
+read_ssh_log()
+
